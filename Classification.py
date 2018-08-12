823cb3968ce4acd2e52a721a0ac4cf00ad8ac53f
WangHao <poopoo1993@gmail.com>
2018-08-12 14:25:19 +0800

    1. Add 60 for feature from master branch

--- Classification.py
+++ Classification.py
@@ -24,7 +24,7 @@ y_data = []
 for line in datas:
 	line.strip('\n')
 	temp =  line.split(" ")
-	x_data.append([int(temp[0]), int(temp[1])])
+	x_data.append([int(temp[0]), int(temp[1]), 60])
 	y_data.append([int(temp[2])])
 del temp
 x_data = np.array(x_data)
@@ -32,12 +32,12 @@ y_data = np.array(y_data)
 
 fin.close()
 
-x = tf.placeholder(tf.float32, [None, 2])
+x = tf.placeholder(tf.float32, [None, 3])
 y = tf.placeholder(tf.float32, [None, 1])
 
 
 #There are two layer
-layer1 = add_layer(x, 2, 10, activation_func = tf.nn.relu6)
+layer1 = add_layer(x, 3, 10, activation_func = tf.nn.relu)
 prediction = add_layer(layer1, 10, 1, activation_func = None)
 
 #loss for train
@@ -65,7 +65,7 @@ y_data = []
 for line in datas:
 	line.strip('\n')
 	temp =  line.split(" ")
-	x_data.append([int(temp[0]), int(temp[1])])
+	x_data.append([int(temp[0]), int(temp[1]), 60])
 	y_data.append([int(temp[2])])
 del temp
 x_data = np.array(x_data)
